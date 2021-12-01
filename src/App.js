20ac048cbc1ef5171789900e16bde67c9acbbef6
munroe1786 <era3munroe@gmail.com>
2021-11-30 22:07:57 -0600

    add useState hook and state to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [fname, setFname] = useState("")
   return (
     <div>
       <h1>React Form Handling</h1>
       <form>
         <label>
-          First Name: <input type="text" />
+          First Name: <input type="text" value="fname" />
         </label>
       </form>
+      <h5>First name: {fname}</h5>
     </div>
   );
 }
