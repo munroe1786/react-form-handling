5697208b855cdaf868bbc0f57c5c10883b8a6157
munroe1786 <era3munroe@gmail.com>
2021-11-30 22:11:54 -0600

    add onChange event handler

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 
 function App() {
   const [fname, setFname] = useState("")
+
+  const handleChange = e => {
+    setFname(e.target.value)
+  }
+
   return (
     <div>
       <h1>React Form Handling</h1>
       <form>
         <label>
-          First Name: <input type="text" value="fname" />
+          First Name: {" "}
+          <input type="text" value="fname" onChange={handleChange}/>
         </label>
       </form>
       <h5>First name: {fname}</h5>
